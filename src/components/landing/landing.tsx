b036d8379138b688ffdf60c2d39e268853311611
ndom91 <yo@ndo.dev>
2026-06-20 12:11:28 +0200

    Update hero star iconbutton size

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -137,8 +137,8 @@ export function LandingPage() {
                   <svg
                     className="ld-btn-icon"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="20"
+                    height="20"
                     viewBox="0 0 24 24"
                     fill="currentColor"
                     stroke="currentColor"
